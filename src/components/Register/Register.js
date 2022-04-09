ba61c142861d272757a399e9450817fc48e4e653
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-09 09:55:48 +0600

    register added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h3>Please Login</h3>
+            <h3>Register Now</h3>
             <form>
+                <input type="text" placeholder="Your Name" />
+                <br /> <br />
                 <input type="email" placeholder="Your Email" />
                 <br /> <br />
                 <input type="password" placeholder="Your Password" /> 
                 <br /> <br />
-                <input type="submit" value="Login" />
+                <input type="submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
